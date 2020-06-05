616c0dd14a44e5fda2ef06e20c8183347dbd0c96
Hesham Mourad <hesham.mourad@capgemini.com>
2020-06-05 12:18:19 -0400

    Add eslint and stylelint

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+/* eslint-disable react/jsx-filename-extension */
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
