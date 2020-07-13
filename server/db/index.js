c801f2688be5742f022a980ca2614d4d7733034a
Hesham Mourad <heshammourad@gmail.com>
2020-07-12 20:50:33 -0400

    Add first query

---
+++ server/db/index.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+const query = (text) => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(text);
+    return result;
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = { query };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const helmet = require('helmet');
+const basicAuth = require('express-basic-auth');
+
+const db = require('./db');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
@@ -20,10 +24,20 @@ if (!isDev && cluster.isMaster) {
   });
 
 } else {
+  const { PASSWORD, USERNAME } = process.env;
+
   const app = express();
 
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+  app.use(helmet());
+
+  app.use(basicAuth({
+    users: {
+      [USERNAME]: PASSWORD,
+    }
+  }))
 
   // Answer API requests.
   app.get('/api', function (req, res) {
@@ -31,12 +45,25 @@ if (!isDev && cluster.isMaster) {
     res.send('{"message":"Hello from the custom server!"}');
   });
 
+  app.get('/api/types', (req, res) => {
+    try {
+      const result = db.query('SELECT * FROM types');
+      const results = {
+        types: result ? result.rows : null,
+      };
+      res.send(results);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+  });
+
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
+  app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
   });
 }
