fb554ed33a8cedc1ec9bd2bd8d4f718844ee3ce6
Hesham Mourad <hesham.mourad@capgemini.com>
2020-07-20 12:18:31 -0400

    Add new type form

--- react-ui/src/pages/Types.jsx
+++ react-ui/src/pages/Types.jsx
@@ -1,23 +1,54 @@
 import withData from '../common/WithData';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const Types = ({ data }) => (
-  <>
-    <h2>Food Types</h2>
-    {data && (
-      <ul>
-        {data.map(({ id, name }) => (
-          <li key={id}>
-            <Link to={`/types/${id}`}>{name}</Link>
-          </li>
-        ))}
-      </ul>
-    )}
-  </>
-);
+const Types = ({ data }) => {
+  const [shouldShowAddPanel, setShouldShowAddPanel] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = document.getElementById('typeName').value;
+    if (name) {
+      // Submit name
+    }
+  };
+
+  return (
+    <>
+      <h2>Food Types</h2>
+      {data && (
+        <ul>
+          {data.map(({ id, name }) => (
+            <li key={id}>
+              <Link to={`/types/${id}`}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Button
+        type="button"
+        onClick={() => setShouldShowAddPanel(true)}
+        disabled={shouldShowAddPanel}
+      >
+        Add New Type
+      </Button>
+      {shouldShowAddPanel && (
+        <Form id="newTypeForm" onSubmit={handleSubmit}>
+          <Form.Group controlId="typeName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Button variant="secondary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      )}
+    </>
+  );
+};
 
 Types.propTypes = {
   data: PropTypes.arrayOf(
