4fce95648f60bb9990342d471857497da2a889ef
Hesham Mourad <heshammourad@gmail.com>
2020-08-09 11:46:04 -0400

    Replace react-bootstrap with Material UI on types page

--- react-ui/src/pages/Types.jsx
+++ react-ui/src/pages/Types.jsx
@@ -1,57 +1,106 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import List from '@material-ui/core/List';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 import { postData } from '../api/api';
 import withData from '../common/WithData';
+import ListItemLink from '../components/ListItemLink';
+
+import './Types.scss';
 
 const Types = ({ data, refreshData }) => {
-  const [shouldShowAddPanel, setShouldShowAddPanel] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [nameError, setNameError] = useState(null);
+  const [dialogError, setDialogError] = useState(null);
+
+  const handleAddNewType = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const clearErrors = () => {
+    setNameError(null);
+    setDialogError(null);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    clearErrors();
+
     const name = document.getElementById('typeName').value;
     if (name) {
       const response = await postData('/types', { name });
       if (response) {
         refreshData();
-        setShouldShowAddPanel(false);
+        setDialogOpen(false);
+      } else {
+        setDialogError('Something went wrong');
       }
+    } else {
+      setNameError('Please enter a name');
     }
   };
 
   return (
-    <>
-      <h2>Food Types</h2>
+    <div className="types">
+      <Typography component="h2" variant="h6">
+        Food Types
+      </Typography>
       {data && (
-        <ul>
-          {data.map(({ id, name }) => (
-            <li key={id}>
-              <Link to={`/types/${id}`}>{name}</Link>
-            </li>
-          ))}
-        </ul>
+        <Paper elevation={0}>
+          <List>
+            {data.map(({ id, name }) => (
+              <ListItemLink to={`/types/${id}`} primary={name} />
+            ))}
+          </List>
+        </Paper>
       )}
       <Button
-        type="button"
-        onClick={() => setShouldShowAddPanel(true)}
-        disabled={shouldShowAddPanel}
+        color="primary"
+        variant="outlined"
+        onClick={handleAddNewType}
+        className="btn-add-new-type"
       >
         Add New Type
       </Button>
-      {shouldShowAddPanel && (
-        <Form id="newTypeForm" onSubmit={handleSubmit}>
-          <Form.Group controlId="typeName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-          <Button variant="secondary" type="submit">
+      <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>New Food Type</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            error={nameError}
+            helperText={nameError}
+            required
+            onChange={clearErrors}
+            margin="dense"
+            id="typeName"
+            label="Name"
+            fullWidth
+          />
+          {dialogError && <DialogContentText color="error">{dialogError}</DialogContentText>}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
             Submit
           </Button>
-        </Form>
-      )}
-    </>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
