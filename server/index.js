a2be1fbafec1ec7bf293e33268d3d0ed4aeeeac2
Hesham Mourad <heshammourad@gmail.com>
2020-08-09 20:45:32 -0400

    Add post item

--- server/index.js
+++ server/index.js
@@ -22,7 +22,9 @@ if (!isDev && cluster.isMaster) {
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`,
+    );
   });
 } else {
   const { PASSWORD, USERNAME } = process.env;
@@ -36,11 +38,13 @@ if (!isDev && cluster.isMaster) {
 
   const router = express.Router();
 
-  router.use(basicAuth({
-    users: {
-      [USERNAME]: PASSWORD,
-    },
-  }));
+  router.use(
+    basicAuth({
+      users: {
+        [USERNAME]: PASSWORD,
+      },
+    }),
+  );
 
   router.use(express.json());
 
@@ -48,57 +52,92 @@ if (!isDev && cluster.isMaster) {
     res.status(200).send();
   });
 
-  router.route('/types/:typeId').get(async (req, res) => {
-    try {
-      const { typeId } = req.params;
-      const typeResult = await db.select('SELECT name FROM type WHERE id = $1', [typeId]);
-      if (!typeResult.rowCount) {
-        res.status(404).send();
-        return;
-      }
-      const itemsResult = await db.select('SELECT * FROM item');
-      res.send({ name: typeResult.rows[0].name, items: itemsResult.rows });
-    } catch (err) {
-      console.error(err.toString());
-      res.status(500).send();
-    }
-  });
-
-  router.route('/types').get(async (req, res) => {
-    try {
-      const result = await db.select('SELECT * FROM type');
-      res.send(result.rows);
-    } catch (err) {
-      console.error(err.toString());
-      res.status(500).send();
-    }
-  }).post(async ({ body: { name } }, res) => {
-    try {
-      if (!name) {
-        console.error('Missing name in request body');
-        res.status(400).send();
-        return;
-      }
-      const result = await db.insert('type', { name }, 'id');
-      if (!result) {
+  router
+    .route('/types/:typeId')
+    .get(async ({ params: { typeId } }, res) => {
+      try {
+        const typeResult = await db.select(
+          'SELECT name FROM type WHERE id = $1',
+          [typeId],
+        );
+        if (!typeResult.rowCount) {
+          res.status(404).send();
+          return;
+        }
+        const itemsResult = await db.select('SELECT * FROM item');
+        res.send({ name: typeResult.rows[0].name, items: itemsResult.rows });
+      } catch (err) {
+        console.error(err.toString());
         res.status(500).send();
-        return;
       }
-      res.send(result.rows);
-    } catch (err) {
-      console.error(err.toString());
-      res.status(500).send();
-    }
-  });
+    })
+    .post(async ({ body: { name }, params: { typeId } }, res) => {
+      try {
+        if (!name) {
+          console.error('Missing name in request body');
+          res.status(400).send();
+          return;
+        }
+        const result = await db.insert(
+          'item',
+          { name, type_id: typeId, last_used_date: new Date() },
+          'id',
+        );
+        if (!result) {
+          res.status(500).send();
+          return;
+        }
+        res.send(result.rows);
+      } catch (err) {
+        console.error(err.toString());
+        res.status(500).send();
+      }
+    });
+
+  router
+    .route('/types')
+    .get(async (req, res) => {
+      try {
+        const result = await db.select('SELECT * FROM type');
+        res.send(result.rows);
+      } catch (err) {
+        console.error(err.toString());
+        res.status(500).send();
+      }
+    })
+    .post(async ({ body: { name } }, res) => {
+      try {
+        if (!name) {
+          console.error('Missing name in request body');
+          res.status(400).send();
+          return;
+        }
+        const result = await db.insert('type', { name }, 'id');
+        if (!result) {
+          res.status(500).send();
+          return;
+        }
+        res.send(result.rows);
+      } catch (err) {
+        console.error(err.toString());
+        res.status(500).send();
+      }
+    });
 
   app.use('/api', router);
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(
+      path.resolve(__dirname, '../react-ui/build', 'index.html'),
+    );
   });
 
   app.listen(PORT, () => {
-    console.error(`Node ${isDev ? 'dev server' : `cluster worker ${process.pid}`}: listening on port ${PORT}`);
+    console.error(
+      `Node ${
+        isDev ? 'dev server' : `cluster worker ${process.pid}`
+      }: listening on port ${PORT}`,
+    );
   });
 }
